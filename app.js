1ca1e18c01117f632e0fafa7e9f4961a042c30c1
Macaco2593 <santiagopjq13@gmail.com>
2022-01-15 17:43:51 -0400

    Añadido boton de show

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-alert('algo')
+/*alert('algo')*/
 
 const Ventana_Emergente =document.getElementById("Ventana_Emergente");
 const cerrar =document.getElementById("cerrar");
+const abrir = document.getElementById("abrir")
+
 
 Ventana_Emergente.classList.add("show");
 
 cerrar.addEventListener("click", () => {
     Ventana_Emergente.classList.remove("show");
     Ventana_Emergente.classList.add("hide");
+})
+
+abrir.addEventListener("click", () =>{
+    Ventana_Emergente.classList.add("show");
+    Ventana_Emergente.classList.remove("hide");
 })
